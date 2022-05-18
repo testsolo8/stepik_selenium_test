a4312fb8abb93d76788379af99e9e590e4ca2ac4
gerfunya <gerfik@ya.ru>
2022-05-18 21:38:17 +0300

    Initial commit

---
+++ pages/main_page.py
@@ -0,0 +1,13 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from pages.locators import MainPageLocators
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        # self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
